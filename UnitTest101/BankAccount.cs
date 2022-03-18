1078ed012c3829e7cba936fbc9c519b93d802138
Deni22e <griseldadenisfernandez@gmail.com>
2022-03-18 19:00:00 -0300

    IntegrationTests

---
+++ UnitTest101/BankAccount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTest101
+{
+	public class BankAccount
+	{
+		public double Balance { get; set; }
+		public BankAccount()
+		{
+			Balance = 0;
+		}
+		public bool Deposit(double amount)
+		{
+			Balance += amount;
+			return true;
+		}
+		public bool Withdraw(double amount)
+		{
+			if (Balance >= amount)
+			{
+				Balance -= amount;
+				return true;
+			}				
+			else
+				return false;
+		}
+		public double GetBalance()
+		{
+			return Balance;
+		}
+	}
+}
---
+++ UnitTest101/LogBook.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTest101
+{
+	public interface ILogger
+	{
+		void Message(string mssg);
+	}
+	public class LogBook : ILogger
+	{
+		public void Message(string mssg)
+		{
+			Console.WriteLine(mssg);
+		}
+	}
+}
