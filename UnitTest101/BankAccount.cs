632f10a3b3e236f0f45bbf478af49d0866535616
Deni22e <griseldadenisfernandez@gmail.com>
2022-03-20 16:44:05 -0300

    Revert "Testing if Moq's method where called  'n prop (set/get)"
    This reverts commit 3e8f0465a41d0953ddc78ff2a29e6d19fc1a2d16.

--- UnitTest101/BankAccount.cs
+++ UnitTest101/BankAccount.cs
@@ -16,8 +16,6 @@ namespace UnitTest101
 		public bool Deposit(double amount)
 		{
 			_logBook.Message("Begin deposit transaction");
-			_logBook.LogSeverity = 1;
-			var temp = _logBook.LogSeverity;
 			Balance += amount;
 			return true;
 		}
--- UnitTest101NUnitTest/BankAccountNUnitTest.cs
+++ UnitTest101NUnitTest/BankAccountNUnitTest.cs
@@ -143,23 +143,6 @@ namespace UnitTest101
 			Assert.That(LogBookMoq.Object.LogSeverity, Is.EqualTo(100));
 			Assert.That(LogBookMoq.Object.LogType, Is.EqualTo("w"));
 		}
-		[Test]
-		public void Deposit_VerifyMoqsProperties()
-		{
-			//Arrange
-			var logBookMoq = new Mock<ILogger>();
-			var account = new BankAccount(logBookMoq.Object);
-			account.Deposit(111);
-			//Verificar si _logBook.Message fue invocado en Deposit() al menos una vez (line 18)
-			logBookMoq.Verify(x => x.Message(It.IsAny<string>()), Times.AtLeastOnce());
-			//Verificar si _logBook.Message fue invocado en Deposit() exactamente una vez  (line 18)
-			logBookMoq.Verify(x => x.Message(It.IsAny<string>()), Times.Exactly(1));
-			//Verificar si _logBook.Severity fue seteado en Deposit() fue invocado una vez (line 19)
-			logBookMoq.VerifySet(x => x.LogSeverity = 1, Times.Once);
-			//Verificar si _logBook.Severity Get en Deposit()fue llamado una vez(line 20)
-			logBookMoq.VerifyGet(x => x.LogSeverity,Times.Once);
-		}
-
-
+		
 	}
 }
